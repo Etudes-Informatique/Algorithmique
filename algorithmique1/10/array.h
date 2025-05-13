839dea326d1be4a6f6f0a606812a8d3b60425928
Ara <115375868+Arthur-Ara@users.noreply.github.com>
2025-05-13 14:43:57 +0200

    Create array.h

---
+++ algorithmique1/10/array.h
@@ -0,0 +1,41 @@
+#ifndef ARRAY__H
+#define ARRAY__H
+
+#include <stdbool.h>
+#include <stddef.h>
+
+//  cond_count : renvoie le nombre de composants d'un tableau qui satisfont une
+//    fonction de test. L'adresse du premier composant du tableau est base, son
+//    nombre de composants est nmemb, la taille de ses composants est size. La
+//    fonction de test est pointée par cond.
+extern size_t cond_count(const void *base, size_t nmemb, size_t size,
+    bool (*cond)(const void *));
+
+//  cond_left_search : recherche la première occurrence d'un composant d'un
+//    tableau qui satisfait une fonction de test. L'adresse du premier composant
+//    du tableau est base, son nombre de composants est nmemb, la taille de ses
+//    composants est size. La fonction de test est pointée par cond. Renvoie
+//    l'adresse du composant trouvé en cas de succès, NULL en cas d'échec.
+extern void *cond_left_search(const void *base, size_t nmemb, size_t size,
+    bool (*cond)(const void *));
+
+//  min_left_search : recherche la première occurrence d'un composant d'un
+//    tableau qui est minimum au sens d'une fonction de comparaison. L'adresse
+//    du premier composant du tableau est base, son nombre de composants est
+//    nmemb, la taille de ses composants est size. La fonction de comparaison
+//    est pointée par compar. Renvoie NULL si nmemb vaut zéro, l'adresse du
+//    composant trouvé sinon.
+extern void *min_left_search(const void *base, size_t nmemb, size_t size,
+    int (*compar)(const void *, const void *));
+
+//  insert_sort, mergebinsteps_sort, select_sort : même spécification que la
+//    fonction qsort déclarée dans l'en-tête standard <stdlib.h>.
+
+extern void insert_sort(void *base, size_t nmemb, size_t size,
+    int (*compar)(const void *, const void *));
+extern void mergebinsteps_sort(void *base, size_t nmemb, size_t size,
+    int (*compar)(const void *, const void *));
+extern void select_sort(void *base, size_t nmemb, size_t size,
+    int (*compar)(const void *, const void *));
+
+#endif
